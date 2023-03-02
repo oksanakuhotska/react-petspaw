278527d94f1f999394298a0f4651081526997e13
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-02 10:46:59 +0200

    feat: added new components

---
+++ src/components/NavCard/NavCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavCard() {
+	return (
+		<div>NavCard</div>
+	)
+}
+
+export default NavCard;
---
+++ src/components/NavCards/NavCards.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavCards() {
+	return (
+		<div>NavCards</div>
+	)
+}
+
+export default NavCards;
