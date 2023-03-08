fa941cc1d52b0be1f434c639c7194a7a6b838883
Yana Shum <yana.sh14@gmail.com>
2023-03-08 18:32:41 +0200

    feat: created map categories

--- src/components/NavCards/NavCards.component.jsx
+++ src/components/NavCards/NavCards.component.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import NavCard from '../NavCard/navCard.component';
+import VoteImg from "../../assets/images/votetable.png";
+import BreedsImg from "../../assets/images/breeds.png";
+import GalleryImg from "../../assets/images/gallery.png";
 
-function NavCards() {
+
+const categories = [
+  {
+    id: 1,
+    title: 'voting',
+    imageUrl: VoteImg,
+    route: 'voting',
+  },
+  {
+    id: 2,
+    title: 'breeds',
+    imageUrl: BreedsImg,
+    route: 'breeds',
+  },
+  {
+    id: 3,
+    title: 'gallery',
+    imageUrl: GalleryImg,
+    route: 'gallery',
+  },
+];
+
+
+
+const NavCards = () => {
 	return (
-		<div>NavCards</div>
+			<div className='control-panel'>
+			
+			{categories.map((category) => (
+        <NavCard key={category.id} category={category} />
+      ))}
+			
+			</div>
 	)
 }
 
