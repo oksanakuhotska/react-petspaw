f16b7c0cfa7ae8ba8413b143fd3fa3e4ab82cc0c
Yana Shum <yana.sh14@gmail.com>
2023-03-08 18:32:12 +0200

    feat: created navCard category

--- src/components/NavCard/navCard.component.jsx
+++ src/components/NavCard/navCard.component.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-function NavCard() {
+const NavCard = ({category}) => {
+	const { imageUrl, title, route } = category;
+	const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
+
 	return (
-		<div>NavCard</div>
+		<>
+			
+			<div>
+				<div>
+
+					<img src={imageUrl} alt={title} />
+						
+				</div>
+				<button onClick={onNavigateHandler}>{title}</button>
+			</div>
+			
+			</>
+
+
 	)
 }
 
 export default NavCard;
+
+
