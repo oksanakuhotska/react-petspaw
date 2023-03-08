827acac7ea4f6bb3f39b37c7f84b12c3c501d98c
Yana Shum <yana.sh14@gmail.com>
2023-03-08 14:11:54 +0200

    style: added styles to navigation

--- src/pages/NavigationPanel/navigationPanel.styles.jsx
+++ src/pages/NavigationPanel/navigationPanel.styles.jsx
@@ -3,3 +3,24 @@ import styled from "styled-components";
 export const NavWrapper = styled.div`
 	
 `;
+
+export const Container = styled.div`
+    gap: 30px;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: space-between;
+  
+`;
+Container.displayName = 'AppContainer';
+
+export const SideBar = styled.div`
+	flex: 0 1 50%;
+`;
+SideBar.displayName = 'AppSideBar';
+
+export const Pages = styled.div`
+  flex: 0 1 50%;
+`;
+
+Pages.displayName = 'AppPages';
