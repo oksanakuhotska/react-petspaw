c1967f0936ce304649b94dc9ebf40244572a1028
Yana Shum <yana.sh14@gmail.com>
2023-03-10 14:00:08 +0200

    refactor: created Card base

--- src/components/NavCard/navCard.component.jsx
+++ src/components/NavCard/navCard.component.jsx
@@ -1,27 +1,42 @@
 import { useNavigate } from 'react-router-dom';
+import { Card, BaseCardBody, CardImage, VotingCardBody, BreedsCardBody, GalleryCardBody } from './navCard.styles';
+
+export const CARD_TYPE_CLASSES = {
+	base: "base",
+	voting: 'voting-card',
+	breeds: 'breeds-card',
+	gallery: 'gallery-card'
+};
+
+const getCardBody = (cardBodyType = CARD_TYPE_CLASSES.base) => (
+	{
+		[CARD_TYPE_CLASSES.base]: BaseCardBody,
+		[CARD_TYPE_CLASSES.voting]: VotingCardBody,
+		[CARD_TYPE_CLASSES.breeds]: BreedsCardBody,
+		[CARD_TYPE_CLASSES.gallery]: GalleryCardBody,
+	}[cardBodyType]
+)
+
+export const CardBody = ({ children, cardBodyType, ...otherProps }) => {
+  const CustomCardBody = getCardBody(cardBodyType);
+
+  return (
+  <CustomCardBody {...otherProps}>{children}</CustomCardBody>
+  );
+};
 
 const NavCard = ({category}) => {
 	const { imageUrl, title, route } = category;
 	const navigate = useNavigate();
-
-  const onNavigateHandler = () => navigate(route);
-
-
+	const onNavigateHandler = () => navigate(route);
+	
 	return (
-		<>
-			
-			<div>
-				<div>
-
-					<img src={imageUrl} alt={title} />
-						
-				</div>
+			<Card>
+				<CardBody>
+					<CardImage src={imageUrl} alt={title} />					
+				</CardBody>
 				<button onClick={onNavigateHandler}>{title}</button>
-			</div>
-			
-			</>
-
-
+			</Card>
 	)
 }
 
