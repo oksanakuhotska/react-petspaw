7a41e139c84ab0928cba68813b8a3cd7245847a4
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 15:10:33 +0200

    refactor: replaced CardBody, added props to CardBo

--- src/components/NavCard/navCard.component.jsx
+++ src/components/NavCard/navCard.component.jsx
@@ -1,39 +1,17 @@
 import { useNavigate } from 'react-router-dom';
-import { Card, BaseCardBody, CardImage, VotingCardBody, BreedsCardBody, GalleryCardBody } from './navCard.styles';
+import CardBody from '../nav-card-body/nav-card-body.component';
 
-export const CARD_TYPE_CLASSES = {
-	base: "base",
-	voting: 'voting-card',
-	breeds: 'breeds-card',
-	gallery: 'gallery-card'
-};
-
-const getCardBody = (cardBodyType = CARD_TYPE_CLASSES.base) => (
-	{
-		[CARD_TYPE_CLASSES.base]: BaseCardBody,
-		[CARD_TYPE_CLASSES.voting]: VotingCardBody,
-		[CARD_TYPE_CLASSES.breeds]: BreedsCardBody,
-		[CARD_TYPE_CLASSES.gallery]: GalleryCardBody,
-	}[cardBodyType]
-)
-
-export const CardBody = ({ children, cardBodyType, ...otherProps }) => {
-  const CustomCardBody = getCardBody(cardBodyType);
-
-  return (
-  <CustomCardBody {...otherProps}>{children}</CustomCardBody>
-  );
-};
+import { Card, CardImage } from './navCard.styles';
 
 const NavCard = ({category}) => {
-	const { imageUrl, title, route } = category;
+	const { imageUrl, title, route, cardBodyType } = category;
 	const navigate = useNavigate();
 	const onNavigateHandler = () => navigate(route);
 	
 	return (
 			<Card>
-				<CardBody>
-					<CardImage src={imageUrl} alt={title} />					
+				<CardBody cardBodyType={cardBodyType} >
+					<CardImage src={imageUrl} alt={title} />
 				</CardBody>
 				<button onClick={onNavigateHandler}>{title}</button>
 			</Card>
