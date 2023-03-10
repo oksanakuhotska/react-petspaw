2e451055718a64d6d02f75705057d91711d4c47b
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 15:07:40 +0200

    refactor: deleted comments

--- src/pages/NavigationPanel/navigationPanel.component.jsx
+++ src/pages/NavigationPanel/navigationPanel.component.jsx
@@ -1,38 +1,26 @@
 import { ReactComponent as PrimaryLogo} from '../../assets/images/logo.svg';
 
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
-import Voting from '../Voting/voting.component';
-import Breeds from '../Breeds/breeds.component';
-import Gallery from '../Gallery/gallery.component';
+// import Voting from '../Voting/voting.component';
+// import Breeds from '../Breeds/breeds.component';
+// import Gallery from '../Gallery/gallery.component';
 import { Container, SideBar, Pages } from './navigationPanel.styles';
-import NavCards from '../../components/NavCards/navCards.component';
+import NavCards from '../../components/NavCards/NavCards.component';
 
 const NavigationPanel = () => {
 	return(
 		<Container>
 			<SideBar>
-
 				<Link to='/'><PrimaryLogo /></Link>
-
 				<div>Dark mode</div>
 				<h1>Hi intern!</h1>
 				<p>Welcome to MI 2022 Front-end test</p>
 				<p>Lets start using The Cat API</p>
-				
-				{/* <NavLink to='/voting'>
-					<Voting />
-				</NavLink>
-				<NavLink to='/breeds'>
-					<Breeds />
-				</NavLink>
-				<NavLink to='/gallery'>
-					<Gallery />
-				</NavLink> */}
 			<NavCards />
 			</SideBar>
 			<Pages>Hello everybody
-          <Outlet/>
+        <Outlet/>
       </Pages>
 		</Container>
 	)
