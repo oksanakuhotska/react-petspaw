40a20e5d521acdd641797ddf35fb3c5746bbbed9
Yana Shum <yana.sh14@gmail.com>
2023-03-10 13:59:34 +0200

    refactor: added cardBodyType to category

--- src/components/NavCards/NavCards.component.jsx
+++ src/components/NavCards/NavCards.component.jsx
@@ -3,7 +3,8 @@ import NavCard from '../NavCard/navCard.component';
 import VoteImg from "../../assets/images/votetable.png";
 import BreedsImg from "../../assets/images/breeds.png";
 import GalleryImg from "../../assets/images/gallery.png";
-
+import { Container } from './navCards.styles';
+import { CARD_TYPE_CLASSES } from '../NavCard/navCard.component';
 
 const categories = [
   {
@@ -11,32 +12,31 @@ const categories = [
     title: 'voting',
     imageUrl: VoteImg,
     route: 'voting',
+    cardBodyType: "CARD_TYPE_CLASSES.voting",
   },
   {
     id: 2,
     title: 'breeds',
     imageUrl: BreedsImg,
     route: 'breeds',
+    cardBodyType: CARD_TYPE_CLASSES.breeds,
   },
   {
     id: 3,
     title: 'gallery',
     imageUrl: GalleryImg,
     route: 'gallery',
+    cardBodyType: CARD_TYPE_CLASSES.gallery,
   },
 ];
 
-
-
 const NavCards = () => {
 	return (
-			<div className='control-panel'>
-			
-			{categories.map((category) => (
-        <NavCard key={category.id} category={category} />
-      ))}
-			
-			</div>
+			<Container>
+        {categories.map((category) => (
+          <NavCard key={category.id} category={category} cardBodyType={category.cardBodyType} />
+        ))}
+			</Container>
 	)
 }
 
