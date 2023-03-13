6ef93b3964ed23243aafa28751c377b74c9541d9
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-13 16:14:05 +0200

    style: added root variables

--- src/global.styles.jsx
+++ src/global.styles.jsx
@@ -23,6 +23,24 @@ export const GlobalStyle = createGlobalStyle`
 
 		transition: var(--transition);
 	}
+
+	:root {
+	--accent-color: #FF868E;
+	--secondary-color: #FBE0DC;
+	--success-color: #97EAB9;
+	--warning-color: #FFD280;
+	--info-color: #B4B7FF;
+	--light-color: #ffffff;
+	--dark-color: #1D1D1D;
+	--medium-grey-color: #8C8C8C;
+	--light-grey-color: #F8F8F7;
+	--white-color: #ffffff;
+
+	--border-radius-base: 20px;
+	--border-radius-secondary: 10px;
+	--transition: all 0.3s ease 0s;
+	--transition-lazy: all 0.8s else 0s;
+}
 `;
 
 
