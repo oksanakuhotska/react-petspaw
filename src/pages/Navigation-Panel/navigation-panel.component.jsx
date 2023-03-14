ce4901a181c0581d274c38e2c4778babb68edc69
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-14 14:26:45 +0200

    refactor: replaced link & dark theme to component

--- src/pages/Navigation-Panel/navigation-panel.component.jsx
+++ src/pages/Navigation-Panel/navigation-panel.component.jsx
@@ -1,22 +1,21 @@
-import { ReactComponent as PrimaryLogo} from '../../assets/images/logo.svg';
+import { Outlet } from 'react-router-dom';
 
-import { Link, Outlet } from 'react-router-dom';
+import NavCards from '../../components/nav-cards/nav-cards.component';
+import SideBarHeader from '../../components/side-bar-header/side-bar-header.component';
 
 import { Container, SideBar, Pages } from './navigation-panel.styles';
-import NavCards from '../../components/nav-cards/nav-cards.component';
 
 const NavigationPanel = () => {
 	return(
 		<Container>
 			<SideBar>
-				<Link to='/'><PrimaryLogo /></Link>
-				<div>Dark mode</div>
+				<SideBarHeader />
 				<h1>Hi intern!</h1>
 				<p>Welcome to MI 2022 Front-end test</p>
 				<p>Lets start using The Cat API</p>
 			<NavCards />
 			</SideBar>
-			<Pages>Hello everybody
+			<Pages>
         <Outlet/>
       </Pages>
 		</Container>
