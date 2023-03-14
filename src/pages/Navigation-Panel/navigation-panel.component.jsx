ba9e8589c9374a1e7e01dba605b6dfca90a7bd6a
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-14 15:53:12 +0200

    refactor: replaced text to sidebar-main component

--- src/pages/Navigation-Panel/navigation-panel.component.jsx
+++ src/pages/Navigation-Panel/navigation-panel.component.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import NavCards from '../../components/nav-cards/nav-cards.component';
 import SideBarHeader from '../../components/side-bar-header/side-bar-header.component';
+import SideBarMain from '../../components/side-bar-main/side-bar-main.component';
 
 import { Container, SideBar, Pages } from './navigation-panel.styles';
 
@@ -10,10 +11,8 @@ const NavigationPanel = () => {
 		<Container>
 			<SideBar>
 				<SideBarHeader />
-				<h1>Hi intern!</h1>
-				<p>Welcome to MI 2022 Front-end test</p>
-				<p>Lets start using The Cat API</p>
-			<NavCards />
+				<SideBarMain />
+				<NavCards />
 			</SideBar>
 			<Pages>
         <Outlet/>
