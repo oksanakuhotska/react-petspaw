9b11d700d3088ca246000328a3d18271b8e70573
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-14 12:52:14 +0200

    styles: changed transition

--- src/global.styles.jsx
+++ src/global.styles.jsx
@@ -9,20 +9,20 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	body {
-		min-width: 320px;
-		overflow: auto;
-		font-size: 16px;
+	min-width: 320px;
+	overflow: auto;
+	font-size: 16px;
 
-		color: var(--dark-color);
-		background-color: var(--light-grey-color);
+	color: var(--dark-color);
+	background-color: var(--light-grey-color);
 
-		font-weight: 400;
-		line-height: 150%;
-		font-style: normal;
-		font-family: Jost;
-		margin: 0;
+	font-weight: 400;
+	line-height: 150%;
+	font-style: normal;
+	font-family: Jost;
+	margin: 0;
 
-		transition: var(--transition);
+	transition: var(--transition);
 	}
 
 	:root {
@@ -39,7 +39,7 @@ export const GlobalStyle = createGlobalStyle`
 
 	--border-radius-base: 20px;
 	--border-radius-secondary: 10px;
-	--transition: all 0.3s ease 0s;
+	--transition: all 0.15s ease 0s;
 	--transition-lazy: all 0.8s else 0s;
 }
 `;
