8ae16a8c315fb5cc6a0b96aedac3e47eb5cb9611
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-14 14:28:47 +0200

    style: added flex elem

--- src/pages/Navigation-Panel/navigation-panel.styles.jsx
+++ src/pages/Navigation-Panel/navigation-panel.styles.jsx
@@ -1,17 +1,13 @@
 import styled from "styled-components";
 
-export const NavWrapper = styled.div`
-	
-`;
-
 export const Container = styled.div`
 
-@import url('https://fonts.googleapis.com/css?family=Jost:regular,500&display=swap');
-
-	gap: 30px;
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	grid-template-rows: 1fr;
+	gap: 30px;
+
+	padding: 30px;
 	height: 100%;
 	width: 100%;
 	justify-content: space-between;
@@ -20,7 +16,15 @@ export const Container = styled.div`
 Container.displayName = 'AppContainer';
 
 export const SideBar = styled.div`
-	/* flex: 0 1 50%; */
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	flex: 0 1 100%;
+
+	@media (min-width: 1170px) {
+		flex: 0 1 50%;
+	}
 `;
 SideBar.displayName = 'AppSideBar';
 
