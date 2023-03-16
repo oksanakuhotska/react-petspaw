61d96d062c1ca835dabdb9ccfaba9f668cc6ea78
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 18:27:11 +0200

    refactor: deleted not needed div

--- src/pages/Home/home.component.jsx
+++ src/pages/Home/home.component.jsx
@@ -4,10 +4,10 @@ import { HomeImage, Overlay } from "./home.styles";
 
 const Home = () => {
 	return (
-		<div>
+		<>
 			<HomeImage src={homeImg} alt="girl-and-pet.png" />
 			<Overlay></Overlay>
-		</div>
+		</>
 	);
 };
 
