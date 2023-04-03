7cefa4979bc6465c3fee4f8d4b81ed8bcf3e1023
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-04-03 21:12:31 +0300

    refactor: added container

--- src/components/page's-header/page's-header.component.jsx
+++ src/components/page's-header/page's-header.component.jsx
@@ -1,16 +1,16 @@
 import Burger from "../burger/burger.component";
 import ReactionsHistory from "../reactions-history/reactions-history.component";
 import Search from "../search/search.component";
+import { Container } from "./page's-header.styles";
 
 const PagesHeader = () => {
 
-
   return (
-    <>
+    <Container>
       <Burger />
       <Search />
       <ReactionsHistory />
-    </>
+    </Container>
   )
 }
 
