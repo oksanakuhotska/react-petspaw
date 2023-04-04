6437344663f9b8be2b8ef131542800041dc31d88
Yana Shum <yana.sh14@gmail.com>
2023-04-04 17:43:06 +0300

    refactor: replaced page's header component

--- src/components/pages/page's-header/page's-header.component.jsx
+++ src/components/pages/page's-header/page's-header.component.jsx
@@ -1,6 +1,6 @@
-import Burger from "../burger/burger.component";
-import ReactionsHistory from "../reactions-history/reactions-history.component";
-import Search from "../search/search.component";
+import Burger from "../../burger/burger.component";
+import ReactionsHistory from "../../reactions-history/reactions-history.component";
+import Search from "../../search/search.component";
 import { Container } from "./page's-header.styles";
 
 const PagesHeader = () => {
