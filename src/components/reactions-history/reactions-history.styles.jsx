0dfacf098e766853c7275dd7413fba35f9480af7
Yana Shum <yana.sh14@gmail.com>
2023-04-04 17:46:40 +0300

    style: styled icon buttons, reactions

--- src/components/reactions-history/reactions-history.styles.jsx
+++ src/components/reactions-history/reactions-history.styles.jsx
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+Container.displayName = "ReactionsHistoryContainer";
