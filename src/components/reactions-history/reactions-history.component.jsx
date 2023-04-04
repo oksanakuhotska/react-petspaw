90b05007fda6cf5bbebcfb90df81a527404cafe6
Yana Shum <yana.sh14@gmail.com>
2023-04-04 17:46:23 +0300

    feat: added icon buttons, reactions

--- src/components/reactions-history/reactions-history.component.jsx
+++ src/components/reactions-history/reactions-history.component.jsx
@@ -1,8 +1,23 @@
+import IconButton, { ICON_BUTTON_TYPE_CLASSES } from "../buttons/iconButton/iconButton.component";
+import SpriteIcon from "../icon/icon.component";
+import { Container } from "./reactions-history.styles";
+
 const ReactionsHistory = () => {
 
 
   return (
-    <></>
+    <Container>
+      <IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.reactionhistory}>
+        <SpriteIcon icon="smile-happy"/>
+      </IconButton>
+      <IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.reactionhistory}>
+        <SpriteIcon icon="smile-like"/>
+      </IconButton>
+      <IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.reactionhistory}>
+        <SpriteIcon icon="smile-sad"/>
+      </IconButton>
+      
+    </Container>
   )
 }
 
