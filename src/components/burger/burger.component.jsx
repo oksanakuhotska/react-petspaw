50eeed5ba1932d5d47a1326c21098fb2feb2e174
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-04-06 18:20:48 +0300

    refactor: added burger component

--- src/components/burger/burger.component.jsx
+++ src/components/burger/burger.component.jsx
@@ -1,8 +1,18 @@
+import BurgerMenu from '../burger-menu/burger-menu.component';
+
+import { BurgerButton, BurgerContainer, ButtonLine } from './burger.styles';
+
 const Burger = () => {
 
-
   return (
-    <></>
+    <BurgerContainer>
+			<BurgerButton to='/'>
+				<ButtonLine />
+				<ButtonLine />
+				<ButtonLine />
+			</BurgerButton>
+			<BurgerMenu />
+		</BurgerContainer>
   )
 }
 
