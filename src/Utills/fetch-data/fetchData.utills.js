7567ef8b163e9a7f2755bed067fea6bde22bc3c1
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-04-12 11:01:44 +0300

    refactor: add test variant of getData

--- src/Utills/fetch-data/fetchData.utills.js
+++ src/Utills/fetch-data/fetchData.utills.js
@@ -1,16 +1,24 @@
-export const url = `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=${process.env.REACT_APP_API_KEY}`;
+// export const url = `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=${process.env.REACT_APP_API_KEY}`;
 
-fetch(url, {
-  headers: {
-    "x-api-key": `${process.env.REACT_APP_API_KEY}`,
-  },
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    const catsData = data;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+export const url = `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`;
+
+// fetch(url, {
+//   headers: {
+//     "x-api-key": `${process.env.REACT_APP_API_KEY}`,
+//   },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     const catsData = data;
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+export const getData = async (url) => {
+	const response = await fetch(url);
+	return await response.json();
+};
--- src/components/cats-directory/cats-directory.component.jsx
+++ src/components/cats-directory/cats-directory.component.jsx
@@ -1,30 +1,38 @@
 import { useState, useEffect } from "react";
-import { url } from "../../Utills/fetch-data/fetchData.utills";
+import { getData, url } from "../../Utills/fetch-data/fetchData.utills";
+
+// `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`
 
 const CatsDirectory = () => {
   const [cats, setCats] = useState([])
 
   useEffect(() => {
-    fetch(url, {
-      headers: {
-        "x-api-key": `${process.env.REACT_APP_API_KEY}`,
-      },
-    })
-  .then(response => response.json())
-  .then(data => setCats(data))
-  .catch(error => console.log(error))
+  //   fetch(url, {
+  //     headers: {
+  //       "x-api-key": `${process.env.REACT_APP_API_KEY}`,
+  //     },
+  //   })
+  // .then(response => response.json())
+  // .then(data => setCats(data))
+  // .catch(error => console.log(error))
+
+	const fetchCats = async () => {
+		const cats = await getData(`https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`);
+		setCats(cats);
+	};
+
+	fetchCats();
 
   }, [])
 
   return (
     <div>
       {cats.map((cats) => {
+
         return (
-          
-        <div>
-           <img src="" alt="" /> 
-        
-        </div>
+					<div key={cats.id}>
+						<img src={cats.url} alt={cats.breeds[0].name} />
+					</div>
         )
       })}
     </div>
