3760d5236426c26d4a754ee6773da9359bd6bf4f
Yana Shum <yana.sh14@gmail.com>
2023-07-14 11:18:22 +0300

    style: added container for styles

---
+++ src/pages/Breeds-Sorted/breeds-sorted.styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`;
+
+Container.displayName = 'PagesHeader';
+
+export const Body = styled.div`
+
+`;
+
+Body.displayName = 'BreedsSortedBody';
