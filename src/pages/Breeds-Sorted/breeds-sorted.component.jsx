59121719c0d5c2779783f3f9c9e4e36f7837b2cb
Yana Shum <yana.sh14@gmail.com>
2023-07-14 11:17:37 +0300

    feat: added buttons for breeds-sorted

--- src/pages/Breeds-Sorted/breeds-sorted.component.jsx
+++ src/pages/Breeds-Sorted/breeds-sorted.component.jsx
@@ -1,16 +1,25 @@
 import { useParams } from "react-router-dom";
-
+import IconButton, { ICON_BUTTON_TYPE_CLASSES } from "../../components/buttons/iconButton/iconButton.component";
+import TextButton, { TEXT_BUTTON_TYPE_CLASSES } from "../../components/buttons/textButton/textButton.component";
+import PagesHeader from "../../components/pages/page's-header/page's-header.component";
+import { Body, Container } from "./breeds-sorted.styles";
 
 const BreedsSorted = () => {
 
 	const { breeds } = useParams();
 
 	return (
-		<>
-		
-			<h1>Hello</h1>
-
-		</>
+		<Container>
+			<PagesHeader />
+			<Body>
+				<div>
+					<IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.base}>-</IconButton>
+					<TextButton buttonType={TEXT_BUTTON_TYPE_CLASSES.base}>BREEDS</TextButton>
+					 <TextButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsid}>28</TextButton>
+				</div>
+				<div></div>
+			</Body>
+    </Container>
 	);
 };
 
