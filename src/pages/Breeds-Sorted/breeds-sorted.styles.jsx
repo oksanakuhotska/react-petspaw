6f4f688f624100153e3ebcd504632dfcf4e2e4e7
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-07-19 19:29:27 +0300

    style: add styles for slide container

--- src/pages/Breeds-Sorted/breeds-sorted.styles.jsx
+++ src/pages/Breeds-Sorted/breeds-sorted.styles.jsx
@@ -11,3 +11,11 @@ export const Body = styled.div`
 `;
 
 Body.displayName = 'BreedsSortedBody';
+
+export const SliderContainer = styled.div`
+	margin: 0 auto 50px;
+	width: 640px;
+	height: 360px;
+`;
+
+SliderContainer.displayName = 'ImageSliderContainer';
