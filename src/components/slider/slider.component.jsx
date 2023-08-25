be3a8eec97ea62f82db8580317d2255450d2f1b8
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-08-25 16:24:54 +0300

    feat: change sliders' buttons r/l

--- src/components/slider/slider.component.jsx
+++ src/components/slider/slider.component.jsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
 
+import SpriteIcon from "../../components/icon/icon.component";
+
 import { Container, SliderImage, LeftArrow, RightArrow, DotsNavigation, Dot } from "./slider.styles";
-
-
 
 const Slider = ({ slides }) => {
 	const [currentIndex, setCurrentIndex] = useState(0);
+
 	const goToPrevious = () => {
 		const isFirstSlide = currentIndex === 0;
 		const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
 		setCurrentIndex(newIndex);
 	};
+
 	const goToNext = () => {
 		const isLastSlide = currentIndex === slides.length - 1;
 		const newIndex = isLastSlide ? 0 : currentIndex + 1;
 		setCurrentIndex(newIndex);
-	}
+	};
 
 	const goToSlide = (slideIndex) => {
 		setCurrentIndex(slideIndex);
-	}
+	};
 
 	return (
 		<Container>
-			<LeftArrow onClick={goToPrevious}>❰</LeftArrow>
-			<RightArrow onClick={goToNext}>❱</RightArrow>
+			<LeftArrow onClick={goToPrevious}>
+				<SpriteIcon icon="arrow-left"/>
+			</LeftArrow>
+			<RightArrow onClick={goToNext}>
+				<SpriteIcon icon="arrow-left"/>
+			</RightArrow>
 			<SliderImage 
 				style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-				>
+			>
 			</SliderImage>
 			<DotsNavigation>
 				{slides.map((slide, slideIndex) => (
