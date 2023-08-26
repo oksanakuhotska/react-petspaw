10f411d905075354300830480c5de411d82d2836
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-08-26 12:57:31 +0300

    refactor: deleted line with not used code

--- src/pages/Navigation-Panel/navigation-panel.component.jsx
+++ src/pages/Navigation-Panel/navigation-panel.component.jsx
@@ -8,7 +8,6 @@ import { Container, SideBar, Pages } from './navigation-panel.styles';
 import PagesHeader from '../../components/pages/page\'s-header/page\'s-header.component';
 
 const NavigationPanel = () => {
-	const isNonMobile = window.innerWidth >= 1100;
 
 	return(
 		<Container>
