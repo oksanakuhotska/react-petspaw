7b2bde02a693d464f6f1aac8f3e55562b7163f62
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-09-13 13:32:23 +0300

    feat: add navigation buttons

--- src/components/pages/page's-body/page's-body.component.jsx
+++ src/components/pages/page's-body/page's-body.component.jsx
@@ -1,12 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 import CatsDirectory from "../../cats-directory/cats-directory.component";
 
-import { Container, Body } from "./page's-body.styles";
+import { Container, Body, Header, BackLink } from "./page's-body.styles";
+import IconButton, { ICON_BUTTON_TYPE_CLASSES } from "../../buttons/iconButton/iconButton.component";
+import SpriteIcon from "../../icon/icon.component";
+import TextButton, { TEXT_BUTTON_TYPE_CLASSES } from "../../buttons/textButton/textButton.component";
 
 const PagesBody = () => {
+	const navigate = useNavigate();
 
   return (
     <Container>
-      <div>HEADER</div>
+      <Header>
+				<IconButton 
+					buttonType={ICON_BUTTON_TYPE_CLASSES.base}
+					onClick={() => navigate(-1)}
+				>
+					<SpriteIcon icon="arrow-left"/>
+				</IconButton>
+				<TextButton buttonType={TEXT_BUTTON_TYPE_CLASSES.base}>
+					<BackLink to="/breeds">BREEDS</BackLink>
+				</TextButton>
+			</Header>
 			<Body>
 				<CatsDirectory />
 			</Body>
