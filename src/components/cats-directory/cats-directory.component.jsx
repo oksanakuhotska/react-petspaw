df17a91895be68d735df2a6a1ad5728ce867e3ea
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-10-07 12:57:17 +0300

    feat: added props, deleted comments

--- src/components/cats-directory/cats-directory.component.jsx
+++ src/components/cats-directory/cats-directory.component.jsx
@@ -1,41 +1,23 @@
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from "react";
+// import { useNavigate } from 'react-router-dom';
+// import { useState, useEffect } from "react";
 
-import { getData, url } from "../../Utills/fetch-data/fetchData.utills";
+// import { getData, url } from "../../Utills/fetch-data/fetchData.utills";
 import { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.component";
 
 import { DirectoryContainer, ImageContainer, CardImageOverlay, CardButtonWrapper, CardLink, CenteredButton} from "./cats-directory.styles";
 
-// `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`
+const CatsDirectory = ({ cats }) => {
+  // const [cats, setCats] = useState([])
 
-const CatsDirectory = () => {
-  const [cats, setCats] = useState([])
-
-  useEffect(() => {
-  //   fetch(url, {
-  //     headers: {
-  //       "x-api-key": `${process.env.REACT_APP_API_KEY}`,
-  //     },
-  //   })
-  // .then(response => response.json())
-  // .then(data => setCats(data))
-  // .catch(error => console.log(error))
-
-	const fetchCats = async () => {
-		const cats = await getData(url);
-		setCats(cats);
-	};
-
-	// const cats = await getData(`https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`);
+  // useEffect(() => {
+  	//const fetchCats = async () => {
+	// 	const cats = await getData(url);
 	// 	setCats(cats);
 	// };
 
-	fetchCats();
+	// fetchCats();
 
-  }, [])
-
-  // const navigate = useNavigate();
-	// const onNavigateHandler = () => navigate("breeds/:breeds"); didn't use
+  // }, [])
 
   return (
     <DirectoryContainer>
