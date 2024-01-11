6b17529a44c22f2a1f2127e115e495d30aa4d184
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-01-11 13:03:15 +0200

    feat: try & catch for cats array

--- src/components/cats-directory/cats-directory.component.jsx
+++ src/components/cats-directory/cats-directory.component.jsx
@@ -6,7 +6,7 @@ import { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.compo
 
 import { DirectoryContainer, ImageContainer, CardImageOverlay, CardButtonWrapper, CardLink, CenteredButton} from "./cats-directory.styles";
 
-const CatsDirectory = () => {
+	const CatsDirectory = () => {
   const [cats, setCats] = useState([])
 
   useEffect(() => {
@@ -19,27 +19,33 @@ const CatsDirectory = () => {
 
   }, [])
 
-  return (
-    <DirectoryContainer>
-      {cats.map((cats) => {
+	console.log(cats);
 
-        return (
-					<ImageContainer key={`${cats.breeds[0].name}-${cats.id}`}>
-            <img src={cats.url} alt={cats.breeds[0].name} />
-            <CardImageOverlay />
-              
-            <CardButtonWrapper>
-              <CardLink 
-								to={`/breeds/${cats.breeds[0].id}`}
-							>
-                <CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>{cats.breeds[0].name}</CenteredButton>
-              </CardLink>
-            </CardButtonWrapper>
-					</ImageContainer>
-        )
-      })}
-    </DirectoryContainer>
-  )
+	return (
+		<DirectoryContainer>
+			{cats.map((cat) => {
+				// Додайте перевірку наявності breeds та breeds[0]
+				if (cat.breeds && cat.breeds[0]) {
+					return (
+						<ImageContainer key={`${cat.breeds[0].name}-${cat.id}`}>
+							<img src={cat.url} alt={cat.breeds[0].name} />
+							<CardImageOverlay />
+							<CardButtonWrapper>
+								<CardLink to={`/breeds/${cat.breeds[0].id}`}>
+									<CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>{cat.breeds[0].name}</CenteredButton>
+								</CardLink>
+							</CardButtonWrapper>
+						</ImageContainer>
+					);
+				} else {
+					// Якщо об'єкт не має breeds або breeds[0], можна обробити цю ситуацію або пропустити цей об'єкт
+					console.warn('Немає breeds або breeds[0] у кота:', cat);
+					return null; // пропустити цей об'єкт
+				}
+			})}
+		</DirectoryContainer>
+	);
+	
 }
 
 export default CatsDirectory;
