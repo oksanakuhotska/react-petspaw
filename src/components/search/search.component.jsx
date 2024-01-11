682c91359b1d10d77675c8a779b41288794d956a
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-01-11 13:01:06 +0200

    feat: create a new functionality of search

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,29 +1,51 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import IconButton, { ICON_BUTTON_TYPE_CLASSES } from '../buttons/iconButton/iconButton.component';
 import SpriteIcon from "../icon/icon.component";
 
 import { SearchContainer, SearchForm, SearchInput } from "./search.styles";
 
-const Search = ({ onChangeHandler }) => {
-	// const [input, setInput] = useState('');
+const Search = ({ onSearchChange }) => {
+	const [searchTerm, setSearchTerm] = useState("");
+
+	useEffect(() => {
+		if (onSearchChange && typeof onSearchChange === 'function') {
+			onSearchChange(searchTerm);
+		}
+	}, [searchTerm, onSearchChange]);
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+	const handleSearch = (e) => {
+    e.preventDefault(); // Зупиняємо стандартну поведінку форми
+    // Викликаємо функцію обробки пошуку з поточним searchTerm
+    if (onSearchChange && typeof onSearchChange === 'function') {
+      onSearchChange(searchTerm);
+    }
+  };
 
   return (
     <SearchContainer> 
       <SearchForm
         id="search-form"
         role="search"
+				onSubmit={handleSearch} // Додаємо обробник події для форми
       >
         <SearchInput
-          type="search"
+          type="text"
           placeholder="Search for breeds by name"
           name="user-search-request"
           aria-label="Search cats"
-					// value={input}
-					// onChange={(e) => setInput(e.target.value)}
-					// onChange={onChangeHandler}
+					value={searchTerm}
+					onChange={handleInputChange}
         />
-        <IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.base} type="submit">
+        <IconButton 
+					buttonType={ICON_BUTTON_TYPE_CLASSES.base} 
+					type="submit"
+					onClick={handleSearch}
+				>
           <SpriteIcon icon="search" />
         </IconButton>
       </SearchForm>
