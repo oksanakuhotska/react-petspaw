4c2c79f5aae5ab2b095a293bf75922743208bdce
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-01-11 13:02:34 +0200

    feat: add try / catch for search

--- src/components/pages/layout/layout.component.jsx
+++ src/components/pages/layout/layout.component.jsx
@@ -6,43 +6,49 @@ import PagesHeader from "../page's-header/page's-header.component";
 import { getData, url } from "../../../Utills/fetch-data/fetchData.utills";
 
 import { Container } from "./layout.styles";
+import Search from "../../search/search.component";
 
 const PagesLayout = () => {
 
-	const [searchField, setSearchField] = useState('');
+	const [searchField, setSearchField] = useState("");
   const [cats, setCats] = useState([]);
   const [filteredCats, setFilterCats] = useState(cats);
 
   useEffect(() => {
+    const fetchCats = async () => {
+      try {
+        const catsData = await getData(url);
+        setCats(catsData);
+      } catch (error) {
+        console.error("Error fetching cats data:", error);
+      }
+    };
 
-		const fetchCats = async () => {
-			const cats = await getData(url);
-			setCats(cats);
-		};
-
-		fetchCats();
+    fetchCats();
   }, []);
 
-
 	useEffect(() => {
-		const newFilteredCats = cats.filter((cat) => {
-			return cat.name && cat.name.trim().toLocaleLowerCase().includes(searchField);
-		});
+		// Логуємо ім'я кожного кота перед фільтрацією
+		cats.forEach((cat) => console.log(cat.breeds[0]?.name));
+		
+		const newFilteredCats = cats.filter(
+  (cat) => cat.breeds[0]?.name.toLowerCase().includes(searchField?.toLowerCase())
+);
 	
 		setFilterCats(newFilteredCats);
 	}, [cats, searchField]);
 
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
+  const onSearchChange = (newSearchTerm) => {
+    setSearchField(newSearchTerm);
   };
-
-
+	
+	console.log(filteredCats);
+	
 
   return (
     <Container>
-      <PagesHeader onChangeHandler={onSearchChange} />
-      <PagesBody cats={filteredCats} />
+      <Search onSearchChange={onSearchChange} />
+      <PagesBody cats={filteredCats || cats} />
     </Container>
   )
 }
