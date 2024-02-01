ebaa9d2983b5c8d7fd7e777ca16c9ea16a4f48d3
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-02-01 11:39:03 +0200

    feat: added sorted table for breeds

--- src/components/cats-directory/cats-directory.component.jsx
+++ src/components/cats-directory/cats-directory.component.jsx
@@ -1,7 +1,8 @@
 // CatsDirectory component
 import { useEffect, useState } from "react";
-import { getData, url } from "../../Utills/fetch-data/fetchData.utills";
+import { getData, urlBreeds } from "../../Utills/fetch-data/fetchData.utills";
 import { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.component";
+import SortTableBreeds from "../sort-table-breeds/sort-table-breeds.component";
 import {
   DirectoryContainer,
   ImageContainer,
@@ -9,50 +10,95 @@ import {
   CardButtonWrapper,
   CardLink,
   CenteredButton,
+	SortTable
 } from "./cats-directory.styles";
 
-const CatsDirectory = ({ cats }) => {
-  const [fetchedCats, setFetchedCats] = useState([]);
+const CatsDirectory = () => {
+	const [breeds, setBreeds] = useState([]);
+  const [filteredBreeds, setFilteredBreeds] = useState([]);
+  const [sortOrder, setSortOrder] = useState('Default');
+  const [count, setCount] = useState(10);
+  const [selectedBreed, setSelectedBreed] = useState('');
 
-  useEffect(() => {
-    const fetchCats = async () => {
-      const catsData = await getData(url);
-      setFetchedCats(catsData);
+	useEffect(() => {
+    const fetchBreeds = async () => {
+			try {
+				const data = await getData(urlBreeds);
+				setBreeds(data);
+			} catch (error) {
+        console.error('Error fetching breeds:', error);
+      }
     };
 
-    fetchCats();
+    fetchBreeds();
   }, []);
 
-  const displayedCats = cats.length > 0 ? cats : fetchedCats;
+	useEffect(() => {
+    let filtered = breeds;
 
-  console.log(displayedCats);
+    if (selectedBreed) {
+      filtered = breeds.filter(
+        (breed) => breed.name.toLowerCase() === selectedBreed.toLowerCase()
+      );
+    }
+
+    let sortedBreeds = [...filtered];
+
+    if (sortOrder === 'A-Z') {
+      sortedBreeds.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortOrder === 'Z-A') {
+      sortedBreeds.sort((a, b) => b.name.localeCompare(a.name));
+    }
+
+    const updatedBreeds = sortedBreeds.slice(0, count);
+    setFilteredBreeds(updatedBreeds);
+  }, [sortOrder, count, selectedBreed, breeds]);
+
+	const handleSortAZ = () => {
+    setSortOrder('A-Z');
+  };
+
+  const handleSortZA = () => {
+    setSortOrder('Z-A');
+  };
 
   return (
-    <DirectoryContainer>
-      {displayedCats.map((cat) => {
-        if (cat.breeds && cat.breeds[0]) {
-          return (
-            <ImageContainer
-              key={`${cat.breeds[0].name}-${cat.id}`}
-              filtered={cat.filtered} // передача пропсу filtered
-            >
-              <img src={cat.url} alt={cat.breeds[0].name} />
-              <CardImageOverlay />
-              <CardButtonWrapper>
-                <CardLink to={`/breeds/${cat.breeds[0].id}`}>
-                  <CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>
-                    {cat.breeds[0].name}
-                  </CenteredButton>
-                </CardLink>
-              </CardButtonWrapper>
-            </ImageContainer>
-          );
-        } else {
-          console.warn("No breeds or breeds[0] in the cat:", cat);
-          return null;
-        }
-      })}
-    </DirectoryContainer>
+		<>
+			{filteredBreeds && filteredBreeds.length > 0 ? (
+				<>
+					<SortTableBreeds
+          breeds={breeds}
+          selectedBreed={selectedBreed}
+          setSelectedBreed={setSelectedBreed}
+          sortOrder={sortOrder}
+          setSortOrder={setSortOrder}
+          count={count}
+          setCount={setCount}
+          handleSortAZ={handleSortAZ}
+          handleSortZA={handleSortZA}
+        ></SortTableBreeds>
+					<DirectoryContainer>
+						{filteredBreeds.map((breed) => (
+							<ImageContainer
+							key={`${breed.name}-${breed.id}`}
+							>
+								<img src={`https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`} alt={breed.name} />
+							<CardImageOverlay />
+							<CardButtonWrapper>
+								<CardLink to={`/breeds/${breed.id}`}>
+									<CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>
+										{breed.name}
+									</CenteredButton>
+								</CardLink>
+							</CardButtonWrapper>
+						</ImageContainer>
+						))}
+					</DirectoryContainer>
+				</>
+			) : (
+				<p>No breeds found.</p>
+			)}
+		</>
   );
 };
 
