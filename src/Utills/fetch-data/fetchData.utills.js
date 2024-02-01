fa5c4bf47daa7e6f479453d5ebfe781b6b5c285f
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-02-01 11:35:08 +0200

    feat: added url with all breeds

--- src/Utills/fetch-data/fetchData.utills.js
+++ src/Utills/fetch-data/fetchData.utills.js
@@ -1,6 +1,11 @@
 // export const url = `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=${process.env.REACT_APP_API_KEY}`;
 
-export const url = `https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`;
+export const url = `https://api.thecatapi.com/v1/images/search?limit=100&has_breeds=1&api_key=live_5kUSd1JtpPS034duZAAySNladWpg9zfRH6nbqcD81xdkmtDXeu3ulbPrFnP58P2A`;
+
+export const urlBreeds = `https://api.thecatapi.com/v1/breeds`;
+
+// const catUrl = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breeds}`;
+
 
 // fetch(url, {
 //   headers: {
