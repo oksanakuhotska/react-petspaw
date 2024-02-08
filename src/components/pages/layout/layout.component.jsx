1fd2b43d272230473d9f32f517e5c5ca13b7900d
Yana Shum <yana.sh14@gmail.com>
2024-02-08 10:43:42 +0200

    Refactor search functionality in layout and header components

--- src/components/pages/layout/layout.component.jsx
+++ src/components/pages/layout/layout.component.jsx
@@ -1,54 +1,14 @@
-import { useEffect, useState } from "react";
-
 import PagesBody from "../page's-body/page's-body.component";
 import PagesHeader from "../page's-header/page's-header.component";
 
-import { getData, url } from "../../../Utills/fetch-data/fetchData.utills";
-
 import { Container } from "./layout.styles";
-import Search from "../../search/search.component";
 
 const PagesLayout = () => {
 
-	const [searchField, setSearchField] = useState("");
-  const [cats, setCats] = useState([]);
-  const [filteredCats, setFilteredCats] = useState(cats);
-
-  useEffect(() => {
-    const fetchCats = async () => {
-      try {
-        const catsData = await getData(url);
-        setCats(catsData);
-      } catch (error) {
-        console.error("Error fetching cats data:", error);
-      }
-    };
-
-    fetchCats();
-  }, []);
-
-	useEffect(() => {
-		// Логуємо ім'я кожного кота перед фільтрацією
-		cats.forEach((cat) => console.log(cat.breeds[0]?.name));
-		
-		const newFilteredCats = cats.filter(
-  (cat) => cat.breeds[0]?.name.toLowerCase().includes(searchField?.toLowerCase())
-);
-	
-		setFilteredCats(newFilteredCats);
-	}, [cats, searchField]);
-
-  const onSearchChange = (newSearchTerm) => {
-    setSearchField(newSearchTerm);
-  };
-	
-	console.log(filteredCats);
-	
-
   return (
     <Container>
-      <Search onSearchChange={onSearchChange} />
-      <PagesBody cats={filteredCats || cats} />
+      <PagesHeader />
+      <PagesBody />
     </Container>
   )
 }
--- src/components/pages/page's-header/page's-header.component.jsx
+++ src/components/pages/page's-header/page's-header.component.jsx
@@ -1,14 +1,51 @@
+import { useEffect, useState } from "react";
 import Burger from "../../burger/burger.component";
 import ReactionsHistory from "../../reactions-history/reactions-history.component";
 import Search from "../../search/search.component";
 import { Container } from "./page's-header.styles";
+import { getData, url } from "../../../Utills/fetch-data/fetchData.utills";
+
 
 const PagesHeader = () => {
 
+  const [searchField, setSearchField] = useState("");
+  const [cats, setCats] = useState([]);
+  const [filteredCats, setFilteredCats] = useState(cats);
+
+  useEffect(() => {
+    const fetchCats = async () => {
+      try {
+        const catsData = await getData(url);
+        setCats(catsData);
+      } catch (error) {
+        console.error("Error fetching cats data:", error);
+      }
+    };
+
+    fetchCats();
+  }, []);
+
+	useEffect(() => {
+		// Логуємо ім'я кожного кота перед фільтрацією
+		cats.forEach((cat) => console.log(cat.breeds[0]?.name));
+		
+		const newFilteredCats = cats.filter(
+  (cat) => cat.breeds[0]?.name.toLowerCase().includes(searchField?.toLowerCase())
+);
+	
+		setFilteredCats(newFilteredCats);
+	}, [cats, searchField]);
+
+  const onSearchChange = (newSearchTerm) => {
+    setSearchField(newSearchTerm);
+  };
+	
+  console.log(filteredCats);
+  
   return (
     <Container>
       <Burger />
-      <Search />
+      <Search onSearchChange={onSearchChange} />
       <ReactionsHistory />
     </Container>
   )
