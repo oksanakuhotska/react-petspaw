30ea25ac8febbcec7317fdaaae40dfc6f8f24e45
Yana Shum <yana.sh14@gmail.com>
2024-02-08 10:25:49 +0200

    refactor: deleted unnecessary component

--- src/pages/Gallery/gallery.component.jsx
+++ src/pages/Gallery/gallery.component.jsx
@@ -1,11 +1,8 @@
-import SortTableBreeds from "../../components/sort-table-breeds/sort-table-breeds.component";
-
 
 const Gallery = () => {
 	return (
 		<>
 			<h2>Gallery</h2>
-			<SortTableBreeds />
 		</>
 	);
 };
