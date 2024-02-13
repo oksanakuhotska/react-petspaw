fce7a9d39113c804c74e6565d019102d9db704d0
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-02-13 22:08:26 +0200

    Refactor: CatsDirectory component

--- src/components/cats-directory/cats-directory.component.jsx
+++ src/components/cats-directory/cats-directory.component.jsx
@@ -1,38 +1,30 @@
-// CatsDirectory component
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getData, urlBreeds } from "../../Utills/fetch-data/fetchData.utills";
-import { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.component";
 import SortTableBreeds from "../sort-table-breeds/sort-table-breeds.component";
-import {
-  DirectoryContainer,
-  ImageContainer,
-  CardImageOverlay,
-  CardButtonWrapper,
-  CardLink,
-  CenteredButton,
-} from "./cats-directory.styles";
+import { DirectoryContainer } from "./cats-directory.styles";
+import CatsGridCard from "../cats-grid-card/cats-grid-card.component";
 
 const CatsDirectory = () => {
-	const [breeds, setBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
   const [filteredBreeds, setFilteredBreeds] = useState([]);
-  const [sortOrder, setSortOrder] = useState('Default');
+  const [sortOrder, setSortOrder] = useState("Default");
   const [count, setCount] = useState(10);
-  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedBreed, setSelectedBreed] = useState("");
 
-	useEffect(() => {
+  useEffect(() => {
     const fetchBreeds = async () => {
-			try {
-				const data = await getData(urlBreeds);
-				setBreeds(data);
-			} catch (error) {
-        console.error('Error fetching breeds:', error);
+      try {
+        const data = await getData(urlBreeds);
+        setBreeds(data);
+      } catch (error) {
+        console.error("Error fetching breeds:", error);
       }
     };
 
     fetchBreeds();
   }, []);
 
-	useEffect(() => {
+  useEffect(() => {
     let filtered = breeds;
 
     if (selectedBreed) {
@@ -43,9 +35,9 @@ const CatsDirectory = () => {
 
     let sortedBreeds = [...filtered];
 
-    if (sortOrder === 'A-Z') {
+    if (sortOrder === "A-Z") {
       sortedBreeds.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortOrder === 'Z-A') {
+    } else if (sortOrder === "Z-A") {
       sortedBreeds.sort((a, b) => b.name.localeCompare(a.name));
     }
 
@@ -53,51 +45,46 @@ const CatsDirectory = () => {
     setFilteredBreeds(updatedBreeds);
   }, [sortOrder, count, selectedBreed, breeds]);
 
-	const handleSortAZ = () => {
-    setSortOrder('A-Z');
+  const handleSortAZ = () => {
+    setSortOrder("A-Z");
   };
 
   const handleSortZA = () => {
-    setSortOrder('Z-A');
+    setSortOrder("Z-A");
   };
 
   return (
-		<>
-			{filteredBreeds && filteredBreeds.length > 0 ? (
-				<>
-					<SortTableBreeds
-						breeds={breeds}
-						selectedBreed={selectedBreed}
-						setSelectedBreed={setSelectedBreed}
-						sortOrder={sortOrder}
-						setSortOrder={setSortOrder}
-						count={count}
-						setCount={setCount}
-						handleSortAZ={handleSortAZ}
-						handleSortZA={handleSortZA}
-					></SortTableBreeds>
-					<DirectoryContainer>
-						{filteredBreeds.map((breed) => (
-							<ImageContainer
-							key={`${breed.name}-${breed.id}`}
-							>
-								<img src={`https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`} alt={breed.name} />
-							<CardImageOverlay />
-							<CardButtonWrapper>
-								<CardLink to={`/breeds/${breed.id}`}>
-									<CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>
-										{breed.name}
-									</CenteredButton>
-								</CardLink>
-							</CardButtonWrapper>
-						</ImageContainer>
-						))}
-					</DirectoryContainer>
-				</>
-			) : (
-				<p>No breeds found.</p>
-			)}
-		</>
+    <>
+      {filteredBreeds && filteredBreeds.length > 0 ? (
+        <>
+          <SortTableBreeds
+            breeds={breeds}
+            selectedBreed={selectedBreed}
+            setSelectedBreed={setSelectedBreed}
+            sortOrder={sortOrder}
+            setSortOrder={setSortOrder}
+            count={count}
+            setCount={setCount}
+            handleSortAZ={handleSortAZ}
+            handleSortZA={handleSortZA}
+          />
+          <DirectoryContainer>
+            {filteredBreeds.map((breed) => (
+              <CatsGridCard 
+								key={breed.id}
+								src={`https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`}
+								alt={breed.name}
+								to={`/breeds/${breed.id}`}
+								buttonText={breed.name}
+								keyProp={breed.id}
+							/>
+            ))}
+          </DirectoryContainer>
+        </>
+      ) : (
+        <p>No breeds found.</p>
+      )}
+    </>
   );
 };
 
