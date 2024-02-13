19cb7cabdb08c3a5887882da720ed47eb124e29e
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-02-13 22:48:42 +0200

    Add button icon support to CatsGridCard component

--- src/components/cats-grid-card/cats-grid-card.component.jsx
+++ src/components/cats-grid-card/cats-grid-card.component.jsx
@@ -1,5 +1,7 @@
-
+import IconButton, { ICON_BUTTON_TYPE_CLASSES } from "../buttons/iconButton/iconButton.component";
 import { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.component";
+import SpriteIcon from "../icon/icon.component";
+
 
 import {
   ImageContainer,
@@ -7,20 +9,29 @@ import {
   CardButtonWrapper,
   CardLink,
   CenteredButton,
+  ButtonIconWrapper,
 } from "./cats-grid-card.styles";
 
-const CatsGridCard = ({ src, alt, to, buttonText, keyProp }) => {
+const CatsGridCard = ({ src, alt, to, buttonText, buttonIcon, buttonType, keyProp }) => {
   return (
     <ImageContainer key={keyProp}>
       <img src={src} alt={alt} />
       <CardImageOverlay />
-      <CardButtonWrapper>
-        <CardLink to={to}>
-          <CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>
-            {buttonText}
-          </CenteredButton>
-        </CardLink>
-      </CardButtonWrapper>
+				{buttonType === TEXT_BUTTON_TYPE_CLASSES.breedsname ? (
+					<CardButtonWrapper>
+					<CardLink to={to}>
+							<CenteredButton buttonType={TEXT_BUTTON_TYPE_CLASSES.breedsname}>
+								{buttonText}
+							</CenteredButton>
+						</CardLink>
+					</CardButtonWrapper>
+				) : (
+					<ButtonIconWrapper>
+						<IconButton buttonType={ICON_BUTTON_TYPE_CLASSES.base}>
+							<SpriteIcon icon={buttonIcon} />
+						</IconButton>
+					</ButtonIconWrapper>
+				)}
     </ImageContainer>
   );
 };
