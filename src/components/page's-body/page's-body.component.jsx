b11ea7514b98605cdb1b1ec11542baa32c1d46f1
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-02-13 21:05:55 +0200

    refactor: Add TabHeader component to Page's Body

--- src/components/page's-body/page's-body.component.jsx
+++ src/components/page's-body/page's-body.component.jsx
@@ -12,6 +12,7 @@ import {
 	Header, 
 	BackLink
 } from "./page's-body.styles";
+import TabHeader from "../tab-header/tab-header.component";
 
 // const PagesBody = ({ breeds, selectedBreed, setSelectedBreed, sortOrder, setSortOrder, count, setCount, handleSortAZ, handleSortZA }) => {
 
@@ -20,17 +21,7 @@ const PagesBody = () => {
 
   return (
     <Container>
-      <Header>
-        <IconButton
-          buttonType={ICON_BUTTON_TYPE_CLASSES.base}
-          onClick={() => navigate(-1)}
-        >
-          <SpriteIcon icon="arrow-left" />
-        </IconButton>
-        <TextButton buttonType={TEXT_BUTTON_TYPE_CLASSES.base}>
-          <BackLink to="/breeds">BREEDS</BackLink>
-        </TextButton>
-      </Header>
+      <TabHeader tabs="Breeds" />
       <Body>
         <CatsDirectory />
       </Body>
@@ -39,3 +30,46 @@ const PagesBody = () => {
 };
 
 export default PagesBody;
+
+
+// // PagesBody component
+// import { useNavigate } from "react-router-dom";
+// import CatsDirectory from "../cats-directory/cats-directory.component";
+// import IconButton, { ICON_BUTTON_TYPE_CLASSES } from "../buttons/iconButton/iconButton.component";
+// import SpriteIcon from "../icon/icon.component";
+// import TextButton, { TEXT_BUTTON_TYPE_CLASSES } from "../buttons/textButton/textButton.component";
+// import SortTableBreeds from "../sort-table-breeds/sort-table-breeds.component";
+
+// import {
+// 	Container, 
+// 	Body, 
+// 	Header, 
+// 	BackLink
+// } from "./page's-body.styles";
+
+// // const PagesBody = ({ breeds, selectedBreed, setSelectedBreed, sortOrder, setSortOrder, count, setCount, handleSortAZ, handleSortZA }) => {
+
+// const PagesBody = () => {
+//   const navigate = useNavigate();
+
+//   return (
+//     <Container>
+//       <Header>
+//         <IconButton
+//           buttonType={ICON_BUTTON_TYPE_CLASSES.base}
+//           onClick={() => navigate(-1)}
+//         >
+//           <SpriteIcon icon="arrow-left" />
+//         </IconButton>
+//         <TextButton buttonType={TEXT_BUTTON_TYPE_CLASSES.base}>
+//           <BackLink to="/breeds">BREEDS</BackLink>
+//         </TextButton>
+//       </Header>
+//       <Body>
+//         <CatsDirectory />
+//       </Body>
+//     </Container>
+//   );
+// };
+
+// export default PagesBody;
