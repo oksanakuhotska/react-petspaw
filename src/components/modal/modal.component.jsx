74b0f3b89c1a1a5caf603376167ce1774f0031e6
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2024-03-26 12:11:41 +0200

    feat: added close click outside modal

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -1,12 +1,32 @@
+import { useRef, useEffect } from "react";
+
 import { ModalWrapper, ModalContent } from "./modal.styles";
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <ModalWrapper>
-      <ModalContent>
-        <h1>MODAL</h1>
+      <ModalContent ref={modalRef}>
+				<h1>MODAL</h1>
         <button onClick={onClose}>Close</button>
       </ModalContent>
     </ModalWrapper>
